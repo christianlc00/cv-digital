1cea2968cc827c249bca21a09b8d8e0050ea3c21
Christian López Carretero <christian@lopezcarretero.es>
2023-08-06 23:44:06 +0200

    CV details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-const routes: Routes = [];
+import { PageComponent } from './components/page/page.component';
+
+const routes: Routes = [
+  { path: '', component: PageComponent },
+  // { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/page/page.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import * as QRCode from 'qrcode';
+
+@Component({
+  selector: 'app-page',
+  templateUrl: './page.component.html',
+  styleUrls: ['./page.component.scss'],
+})
+export class PageComponent implements OnInit {
+  public qrImage = '';
+  public pColor = '232d4bff';
+  public sColor = '00aa9bff';
+  public language = '';
+
+  constructor(private _activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    QRCode.toDataURL('https://christianlc.dev/', {
+      margin: 1,
+      type: 'image/png',
+    })
+      .then((url) => {
+        this.qrImage = url;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+}
